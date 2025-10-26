ebcd06215903952c28902a0632fb1e785557a415
Max Veksler <maximvek05@gmail.com>
2025-10-25 20:21:50 -0400

    increased FPS

--- real_code/audio-reactive-led-strip/python/config.py
+++ real_code/audio-reactive-led-strip/python/config.py
@@ -78,8 +78,8 @@ GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
 MIC_RATE = 16000
 """Sampling frequency of the microphone in Hz"""
 
-# FPS = 40
-FPS = 27
+FPS = 40
+# FPS = 27
 """Desired refresh rate of the visualization (frames per second)
 
 FPS indicates the desired refresh rate, or frames-per-second, of the audio
